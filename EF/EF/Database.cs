cf22ac51f25d5498921513b4a2f3f1705806bccb
Elou-Zamkol <raulabdurahmanli2006@gmail.com>
2025-02-23 18:15:51 +0400

    Add files via upload

---
+++ EF/EF/Database.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+static class Database
+{
+   public static void AddCar(Car car, string connectionString)
+    {
+        var NewCar = "INSERT INTO Cars (Brand, Model, Year, Price)" + "VALUES (@Brand, @Model, @Year, @Price)" + "SELECT CAST(SCOPE_IDENTITY() AS INT)";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var newCarId = connection.Execute(NewCar, new { Brand = car.Brand, Model = car.Model, Year = car.Year, Price = car.Price });
+        }
+    }
+
+    public static bool RemoveCar(int Id, string connectionString)
+    {
+        var commandString = "DELETE FROM Cars WHERE Id = @CarId";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var newCarId = connection.Execute(commandString, new { Id = Id });
+            return true;
+        }
+
+    }
+
+    public static bool UpdateCar(int Id,decimal Price, string connectionString)
+    {
+        
+        var commandString = "UPDATE Cars SET Price = @NewPrice WHERE Id = @CarId";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var newCarId = connection.Execute(commandString, new { Id = Id, Price = Price });
+            return true;
+        }
+        
+    }
+
+    public static void ShowCars(string connectionString)
+    {
+        var commandString = "select * from Cars";
+        using var connection = new SqlConnection(connectionString);
+        connection.Open();
+        using var command = new SqlCommand(commandString, connection);
+        using var reader = command.ExecuteReader();
+        Console.WriteLine($"");
+        while (reader.Read())
+        {
+            Console.WriteLine($"Id- {reader["Id"]}  Brand- {reader["Brand"]}  Model- {reader["Model"]}  Year- {reader["Year"]}  Price- {reader["Price"]}");
+        }
+    }
+    
+    public static void ShowCarsFromBrand(string Brand, string connectionString)
+    {
+        var commandString = "SELECT * FROM Cars WHERE Brand = @Brand";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var reader = connection.ExecuteReader(commandString, new { Brand = Brand });
+            while (reader.Read())
+            {
+                Console.WriteLine($"Id- {reader["Id"]}  Brand- {reader["Brand"]}  Model- {reader["Model"]}  Year- {reader["Year"]}  Price- {reader["Price"]}");
+            }
+        }
+    }
+}
---
+++ EF/EF/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+
+var connectionString = "Data Source=localhost; Initial Catalog=Academy1; Integrated Security=true; Trust Server Certificate=true";
+
+while (true)
+{
+    Console.WriteLine("");
+    short choice;
+    Console.WriteLine("1 - Add Car\n2 - Update Car\n3 - Remove Car\n4 - Output Car \n5 - Output Car for Brand.");
+    Console.Write("Your choice: ");
+    choice = Convert.ToInt16(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        Console.Clear();
+        Console.Write("Enter your Brand: ");
+        string Brand = Console.ReadLine();
+        Console.Write("Enter your Model: ");
+        string Model = Console.ReadLine();
+        Console.Write("Enter your Year: ");
+        int Year = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your Price: ");
+        decimal Price = Convert.ToDecimal(Console.ReadLine());
+        
+        Car car = new Car(Brand, Model, Year, Price);
+        Database.AddCar(car, connectionString);
+        Console.Clear();
+
+    }
+
+    else if (choice == 2)
+    {
+        Console.Clear();
+        Console.Write("Enter ID: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter ID: ");
+        decimal NewPrice = Convert.ToDecimal(Console.ReadLine());
+        
+        Database.UpdateCar(id, NewPrice, connectionString);
+        Console.Clear();
+        
+    }
+
+    else if (choice == 3)
+    {
+        Console.Clear();
+        Console.Write("Enter ID: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+        
+        Database.RemoveCar(id, connectionString);
+        Console.Clear();
+    }
+
+    else if (choice == 4)
+    {
+        Console.Clear();
+        Database.ShowCars(connectionString);
+    }
+    
+    else if (choice == 5)
+    {
+        Console.Clear();
+        Console.Write("Enter your Brand: ");
+        string Brand = Console.ReadLine();
+        Database.ShowCarsFromBrand(Brand, connectionString);
+    }
+
+    else
+    {
+        break;
+    }
+
+}
